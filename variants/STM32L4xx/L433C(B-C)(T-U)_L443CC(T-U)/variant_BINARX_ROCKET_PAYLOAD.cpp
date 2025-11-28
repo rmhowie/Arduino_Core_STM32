75370f963370a3bc245df8a0263328d9c3452254
rmhowie <rmhowie@sstc>
2025-11-28 20:53:54 +0800

    WIP, think I've finished the .cpp

--- variants/STM32L4xx/L433C(B-C)(T-U)_L443CC(T-U)/variant_BINARX_ROCKET_PAYLOAD.cpp
+++ variants/STM32L4xx/L433C(B-C)(T-U)_L443CC(T-U)/variant_BINARX_ROCKET_PAYLOAD.cpp
@@ -14,8 +14,6 @@
 #if defined(ARDUINO_BINARX_ROCKET_PAYLOAD)
 #include "pins_arduino.h"
 
-// RMH Todo lists below
-
 // Digital PinName array
 const PinName digitalPin[] = {
   PB_10,   // 0 - LPUART1_RX
@@ -24,48 +22,52 @@ const PinName digitalPin[] = {
   PB_13,   // 3 - SPI2_SCK
   PB_14,   // 4 - SPI2_POCI
   PB_15,   // 5 - SPI2_PICO
-  PA_8,   // 6 - USER_SW
-  PA_9,   // 7 - DEBUG_UART_TX
+  PA_8,    // 6 - USER_SW
+  PA_9,    // 7 - DEBUG_UART_TX
   PA_10,   // 8 - DEBUG_UART_RX
-  // RMH Todo lists below
-  PB_14,  // 9 - D9
-  PB_13,  // 10 - D10
-  PB_0,   // 11 - D11
-  PB_15,  // 12 - D12
-  PB_4,   // 13 - D13
-  PA_5,   // 14 - CK
-  PA_6,   // 15 - MI
-  PA_7,   // 16 - A5
-  PA_9,   // 17 - TX
-  PA_10,  // 18 - RX
-  PA_11,  // 19 - USB_DM
-  PA_12,  // 20 - USB_DP
-  PA_13,  // 21 - SWDIO
-  PA_14,  // 22 - SWCLK
-  PA_15,  // 23 - CHARGE_DETECT
-  PB_3,   // 24 - USB_DETECT
-  PB_5,   // 25 - MO
-  PB_6,   // 26 - SCL
-  PB_7,   // 27 - SDA
-  PB_10,  // 28 - LPUART1_VCP_RX
-  PB_11,  // 29 - LPUART1_VCP_TX
-  PC_13,  // 30 - USER_BTN
-  PC_14,  // 31 - OSC32_IN
-  PC_15,  // 32 - OSC32_OUT
-  PH_0,   // 33 - ENABLE_3V3
-  PH_1,   // 34 - DISCHARGE_3V3
-  PH_3    // 35 - B
+  PA_15,   // 9 - D9
+  PB_4,    // 10 - D10
+  PB_5,    // 11 - D11
+  PB_6,    // 12 - I2C1_SCL
+  PB_7,    // 13 - I2C1_SDA
+  PB_8,    // 14 - PB8
+  PB_9,    // 15 - PB9
+  PB_2,    // 16 - PB2
+  PC_13,   // 17 - CARD_DETECT
+  PH_3,    // 18 - BOOT0
+  PA_11,   // 19 - USB_D_N
+  PA_12,   // 20 - USB_D_P
+  PA_13,   // 21 - SWDIO
+  PA_14,   // 22 - SWCLK
+  PB_3,    // 23 - SWO
+  PC_14,   // 24 - OSC_32_IN
+  PC_15,   // 25 - MOSC_32_OUT
+  PH_0,    // 26 - OSC_IN
+  PH_1,    // 27 - OSC_OUT
+  PA_0,    // 28 - ADC1_IN5
+  PA_1,    // 29 - ADC1_IN6
+  PA_2,    // 30 - ADC1_IN7
+  PA_3,    // 31 - ADC1_IN8
+  PA_4,    // 32 - ADC1_IN9
+  PA_5,    // 33 - SPI1_SCK
+  PA_6,    // 34 - SPI1_POCI
+  PA_7,    // 35 - SPI1_PICO
+  PB_0,    // 34 - SPI1_CS
+  PB_1     // 35 - PB1
 };
 
 // Analog (Ax) to digital pin number array
 const uint32_t analogInputPin[] = {
-  0,  // PA0, A0
-  1,  // PA1, A1
-  2,  // PA2, A2
-  3,  // PA3, A3
-  4,  // PB1, A4
-  16, // PA7, A5
-  7   // PA4, BAT_VOLTAGE
+  28,  // PA0, ADC1_IN5,  A0
+  29,  // PA1, ADC1_IN6,  A1
+  30,  // PA2, ADC1_IN7,  A2
+  31,  // PA3, ADC1_IN8,  A3
+  32,  // PA4, ADC1_IN9,  A4
+  33,  // PA5, SPI1_SCK,  A5
+  34,  // PA6, SPI1_POCI, A6
+  35,  // PA7, SPI1_PICO, A7
+  36,  // PB0, SPI1_CS,   A8
+  37   // PB1, PB1,       A9
 };
 
 // ----------------------------------------------------------------------------
